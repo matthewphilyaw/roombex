39a71ab35c50f9ccf44a6f5858146172b3a55a1b
Matthew Philyaw <matthew.philyaw@gmail.com>
2015-04-29 22:31:09 +0000

    Moved Roomba module up to Roombex, starting to see that this really should just be a library that I include in another project.

--- lib/roombex.ex
+++ lib/roombex.ex
@@ -1,11 +1,132 @@
 defmodule Roombex do
-  def init() do
-    {:ok, pid} = Roomba.start_link
-    Roomba.open_port pid, "/dev/pts/6", 57600
-    Roomba.start pid 
-    Roomba.drive pid, 500, :straight
-    Roomba.drive pid, 500, :clockwise
-    Roomba.drive pid, 500, :counter_clockwise
-    Roomba.drive pid, 500, -550
+  require Logger
+  use GenServer
+  
+  @cport_name "priv_dir/roomba_port"
+  
+  @doc """
+  Start Roomba server 
+  """
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, name: :roomba_server)
+  end
+  
+  @doc """
+  Used to open serial port
+  """
+  def open_port(port_name, baud_rate) do
+    Logger.debug "opening port - name: #{port_name} baud rate: #{baud_rate}" 
+    
+    GenServer.call(:roomba_server, {:open, port_name, baud_rate})
+  end
+  
+  @doc """
+  Start command for roomba
+  """
+  def start do
+    Logger.debug "start"
+    
+    GenServer.call(:roomba_server,
+                   {:send,  << 128 :: size(1)-big-integer-unsigned-unit(8) >>}) 
+  end
+  
+  @doc """
+  Drive command for roomba
+  """
+  def drive(speed, angle) when 
+    is_integer(speed) and speed >= -500 and speed <= 500 and
+    ((is_atom(angle) and angle in [:straight, :clockwise, :counter_clockwise]) or
+    (is_integer(angle) and angle >= -2000 and angle <= 2000)) do
+         
+    # We know we have valide angle
+    # now lets transform the few special cases
+    # the actual values roomba needs
+    angle = case angle do
+      :straight -> 0x8000
+      :clockwise -> -1
+      :counter_clockwise -> 1
+      _ -> angle
+    end
+    
+    Logger.debug "drive - speed: #{speed} angle: #{angle}"
+    
+    GenServer.call(:roomba_server, 
+                   {:send, << 137 :: size(1)-big-integer-unsigned-unit(8),
+                            speed :: size(2)-big-integer-signed-unit(8),
+                            angle :: size(2)-big-integer-signed-unit(8) >>})
+  end
+  
+  # Server Call Backs ----------------------------------------------------------
+  
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    port = Port.open({:spawn, @cport_name}, [{:packet, 2}, :binary])
+    
+    Logger.debug "created port"
+    
+    {:ok, {port, :not_ready}}
+  end 
+  
+  def handle_call({type, _}, _, state={_, :not_ready}) when 
+    is_atom(type) and type in [:send, :send_read] do
+    
+    msg = "serial port has not been setup, call open_port"
+    
+    Logger.error msg
+    {:reply, {:error, msg}, state}
+  end
+  
+  def handle_call({:open, _, _}, _from, state={_, :ready}) do
+    msg = "port is already open" 
+    
+    Logger.error msg
+    {:reply, {:error, msg}, state}
+  end
+  
+  def handle_call({:open, port_name, baud_rate}, _from, {port, :not_ready}) do
+    Logger.debug "opening serial port"
+    
+    Port.command(port, :erlang.term_to_binary({:open, port_name, baud_rate}))
+    {:reply, :ok, {port, :ready}}
+  end
+  
+  def handle_call({:send, command}, _from, state={port, :ready}) do
+    hexified = Hexate.encode command
+    Logger.debug "sending command: #{hexified}"
+    
+    Port.command(port, :erlang.term_to_binary({:send, command}))
+    {:reply, :ok, state}
+  end
+  
+  def handle_call({:send_read, command}, _from, state={port, :ready}) do
+    hexified = Hexate.encode command
+    Logger.debug "sending sensor command: #{hexified}"
+    
+    Port.command(port, :erlang.term_to_binary({:send_read, command}))
+    {:reply, :ok, state}
+  end
+  
+  def handle_info({_port, {:data, data}} , state={_port, _}) do
+    msg = :erlang.binary_to_term(data)
+    Logger.debug "received - #{msg}"
+    
+    case msg do
+      :ok -> 
+        {:noreply, state}
+      _ -> 
+        {:stop, msg, state} 
+    end
+  end
+  
+  def handle_info(msg, state) do
+    Logger.debug "recieved message - #{msg}" 
+    
+    {:noreply, state}
+  end
+  
+  def handle_info({:EXIT, port, _}, state) do
+    Logger.error "port exited"
+    
+    {:stop, "port exited", state}
   end
 end
