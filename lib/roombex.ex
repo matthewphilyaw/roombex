3e14d3804d11efd42017567e6447e3ce4ad52a4d
Matthew Philyaw <matthew.philyaw@gmail.com>
2015-05-08 23:24:19 -0400

    Refactored a bit changed the calling semantics to use a do_commands function instead of individually.
    This may prove to be more useful than calling individual functions

--- lib/roombex.ex
+++ lib/roombex.ex
@@ -4,67 +4,159 @@ defmodule Roombex do
   
   @cport_name "priv_dir/roomba_port"
   
-  @doc """
-  Start Roomba server 
-  """
-  def start_link(opts \\ []) do
+  def start_link() do
     GenServer.start_link(__MODULE__, :ok, name: :roomba_server)
   end
   
-  @doc """
-  Used to open serial port
-  """
   def open_port(port_name, baud_rate) do
     Logger.debug "opening port - name: #{port_name} baud rate: #{baud_rate}" 
     
     GenServer.call(:roomba_server, {:open, port_name, baud_rate})
   end
-  
-  @doc """
-  Start command for roomba
-  """
-  def start do
-    Logger.debug "start"
-    
-    GenServer.call(:roomba_server,
-                   {:send,  << 128 :: size(1)-big-integer-unsigned-unit(8) >>}) 
+
+  def do_commands([]) do
+    :ok
+  end
+
+  def do_commands(commands) when is_list(commands) do
+    GenServer.cast(:roomba_server, {:do_commands, commands})
+    :ok
   end
   
-  def power do
-    Logger.debug "start"
+  # Server Call Backs ----------------------------------------------------------
+  
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    port = Port.open({:spawn, @cport_name}, [{:packet, 2}, :binary])
     
-    GenServer.call(:roomba_server,
-                   {:send,  << 133 :: size(1)-big-integer-unsigned-unit(8) >>}) 
+    Logger.debug "created port"
+    
+    {:ok, {port, :not_ready}}
+  end 
+
+  # think I need this to say complete
+  def handle_cast({:do_commands, []}, state) do
+    {:noreply, state}
+  end
+
+  def handle_cast({:do_commands, [{:sleep, value}|rest]}, state) do
+    Logger.debug "sleep - #{value}"
+
+    :timer.sleep value
+    
+    # async - recursively call do_commands untill no more are in the list
+    GenServer.cast(:roomba_server, {:do_commands, rest})
+
+    {:noreply, state}
+  end
+
+  def handle_cast({:do_commands, [h|rest]}, state) do
+    {:ok, parsed_command} = create_command h 
+
+    GenServer.cast(:roomba_server, parsed_command)
+    GenServer.cast(:roomba_server, {:do_commands, rest})
+
+    {:noreply, state}
   end
   
-  def enable_control_mode do
+  def handle_cast({type, _}, state={_, :not_ready}) when 
+    is_atom(type) and type in [:send, :send_read] do
+    
+    msg = "serial port has not been setup, call open_port"
+    
+    Logger.error msg
+    {:noreply, state}
+  end
+  
+  def handle_call({:open, _, _}, _from, state={_, :ready}) do
+    msg = "port is already open" 
+    
+    Logger.error msg
+    {:reply, {:error, msg}, state}
+  end
+  
+  def handle_call({:open, port_name, baud_rate}, _from, {port, :not_ready}) do
+    Logger.debug "opening serial port"
+    
+    Port.command(port, :erlang.term_to_binary({:open, port_name, baud_rate}))
+    
+    {:reply, :ok, {port, :ready}}
+  end
+  
+  def handle_cast({:send, command}, state={port, :ready}) do
+    hexified = Hexate.encode command
+    Logger.debug "sending command - #{hexified}"
+    
+    Port.command(port, :erlang.term_to_binary({:send, command}))
+    {:noreply, state}
+  end
+  
+  def handle_cast({:send_read, command}, state={port, :ready}) do
+    hexified = Hexate.encode command
+    Logger.debug "sending sensor command - #{hexified}"
+    
+    Port.command(port, :erlang.term_to_binary({:send_read, command}))
+    {:noreply, state}
+  end
+  
+  def handle_info({port, {:data, data}} , state={port, _}) do
+    msg = :erlang.binary_to_term(data)
+    Logger.debug "received - #{msg}"
+    
+    case msg do
+      :ok -> 
+        {:noreply, state}
+      _ -> 
+        {:stop, msg, state} 
+    end
+  end
+  
+  def handle_info({:EXIT, _port, _}, state) do
+    Logger.error "port exited"
+    
+    {:stop, "port exited", state}
+  end
+
+  def handle_info(msg, state) do
+    Logger.debug "recieved message - #{msg}" 
+    
+    {:noreply, state}
+  end
+
+  # private 
+  #----------------------------------------------------------------------------
+
+  defp create_command(:start) do
+    Logger.debug "start"
+    
+    {:ok, {:send,  << 128 :: size(1)-big-integer-unsigned-unit(8) >>}}
+  end
+  
+  defp create_command(:power) do
+    Logger.debug "start"
+
+    {:ok, {:send,  << 133 :: size(1)-big-integer-unsigned-unit(8) >>}}
+  end
+  
+  defp create_command(:enable_control_mode) do
     Logger.debug "enable user mode"
      
-     
-    GenServer.call(:roomba_server,
-                   {:send,  << 130 :: size(1)-big-integer-unsigned-unit(8) >>}) 
+    {:ok, {:send,  << 130 :: size(1)-big-integer-unsigned-unit(8) >>}}
   end
   
-  def enable_safe_mode do
+  defp create_command(:enable_safe_mode) do
     Logger.debug "enable safe mode"
      
-     
-    GenServer.call(:roomba_server,
-                   {:send,  << 131 :: size(1)-big-integer-unsigned-unit(8) >>}) 
+    {:ok, {:send,  << 131 :: size(1)-big-integer-unsigned-unit(8) >>}}
   end
   
-  def enable_full_mode do
+  defp create_command(:enable_full_mode) do
     Logger.debug "enable safe mode"
      
-     
-    GenServer.call(:roomba_server,
-                   {:send,  << 132 :: size(1)-big-integer-unsigned-unit(8) >>}) 
+    {:ok, {:send,  << 132 :: size(1)-big-integer-unsigned-unit(8) >>}}
   end
   
-  @doc """
-  Drive command for roomba
-  """
-  def drive(speed, angle) when 
+  defp create_command({:drive, speed, angle}) when 
     is_integer(speed) and speed >= -500 and speed <= 500 and
     ((is_atom(angle) and angle in [:straight, :clockwise, :counter_clockwise]) or
     (is_integer(angle) and angle >= -2000 and angle <= 2000)) do
@@ -84,83 +176,9 @@ defmodule Roombex do
     end
     
     
-    GenServer.call(:roomba_server, 
-                   {:send, << 137 :: size(1)-big-integer-unsigned-unit(8),
-                            speed :: size(2)-big-integer-signed-unit(8),
-                            angle :: size(2)-big-integer-signed-unit(8) >>})
+    {:ok,  {:send, << 137 :: size(1)-big-integer-unsigned-unit(8),
+                      speed :: size(2)-big-integer-signed-unit(8),
+                      angle :: size(2)-big-integer-signed-unit(8) >>}}
   end
-  
-  # Server Call Backs ----------------------------------------------------------
-  
-  def init(:ok) do
-    Process.flag(:trap_exit, true)
-    port = Port.open({:spawn, @cport_name}, [{:packet, 2}, :binary])
-    
-    Logger.debug "created port"
-    
-    {:ok, {port, :not_ready}}
-  end 
-  
-  def handle_call({type, _}, _, state={_, :not_ready}) when 
-    is_atom(type) and type in [:send, :send_read] do
-    
-    msg = "serial port has not been setup, call open_port"
-    
-    Logger.error msg
-    {:reply, {:error, msg}, state}
-  end
-  
-  def handle_call({:open, _, _}, _from, state={_, :ready}) do
-    msg = "port is already open" 
-    
-    Logger.error msg
-    {:reply, {:error, msg}, state}
-  end
-  
-  def handle_call({:open, port_name, baud_rate}, _from, {port, :not_ready}) do
-    Logger.debug "opening serial port"
-    
-    Port.command(port, :erlang.term_to_binary({:open, port_name, baud_rate}))
-    {:reply, :ok, {port, :ready}}
-  end
-  
-  def handle_call({:send, command}, _from, state={port, :ready}) do
-    hexified = Hexate.encode command
-    Logger.debug "sending command - #{hexified}"
-    
-    Port.command(port, :erlang.term_to_binary({:send, command}))
-    {:reply, :ok, state}
-  end
-  
-  def handle_call({:send_read, command}, _from, state={port, :ready}) do
-    hexified = Hexate.encode command
-    Logger.debug "sending sensor command - #{hexified}"
-    
-    Port.command(port, :erlang.term_to_binary({:send_read, command}))
-    {:reply, :ok, state}
-  end
-  
-  def handle_info({_port, {:data, data}} , state={_port, _}) do
-    msg = :erlang.binary_to_term(data)
-    Logger.debug "received - #{msg}"
-    
-    case msg do
-      :ok -> 
-        {:noreply, state}
-      _ -> 
-        {:stop, msg, state} 
-    end
-  end
-  
-  def handle_info(msg, state) do
-    Logger.debug "recieved message - #{msg}" 
-    
-    {:noreply, state}
-  end
-  
-  def handle_info({:EXIT, port, _}, state) do
-    Logger.error "port exited"
-    
-    {:stop, "port exited", state}
-  end
+
 end
