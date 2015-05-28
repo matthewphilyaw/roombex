64dda8cc54989ad6f5559e907c5919742b8f4969
Matthew Philyaw <matthew.philyaw@gmail.com>
2015-05-28 07:53:18 -0400

    using protocols to handle command transformation, cleaner and easier to test.

---
+++ lib/roombex/command.ex
@@ -0,0 +1,75 @@
+defprotocol Roombex.Command do
+  def transform(command)
+end
+
+defimpl Roombex.Command, for: Atom do
+  def transform(:start) do
+    {:ok, << 128 :: size(1)-big-integer-unsigned-unit(8) >>}
+  end 
+  
+  def transform(:control_mode) do
+    {:ok, << 130 :: size(1)-big-integer-unsigned-unit(8) >>}
+  end
+  
+  def transform(:safe_mode) do
+    {:ok, << 131 :: size(1)-big-integer-unsigned-unit(8) >>}
+  end
+  
+  def transform(:full_mode) do
+    {:ok, << 132 :: size(1)-big-integer-unsigned-unit(8) >>}
+  end
+
+  def transform(:power) do
+    {:ok, << 133 :: size(1)-big-integer-unsigned-unit(8) >>}
+  end
+
+  def transform(_) do
+    {:error, "uknown command"}
+  end
+end
+
+defmodule Drive do
+  defstruct speed: 0, angle: :straight
+
+  defimpl Roombex.Command do
+    def transform(%Drive{speed: s}) when is_integer(s) and s < -500 do
+      {:error, 
+       "speed must be greater than or equal to -500. #{s} was supplied"}   
+    end
+
+    def transform(%Drive{speed: s}) when is_integer(s) and s > 500 do
+      {:error, 
+       "speed must be less than or equal to 500. #{s} was supplied"}   
+    end
+
+    def transform(%Drive{angle: a}) when is_integer(a) and a < -2000 do
+      {:error, 
+       "angle must be greater than or equal to 2000. #{a} was supplied"}   
+    end
+
+    def transform(%Drive{angle: a}) when is_integer(a) and a > 2000 do
+      {:error, 
+       "angle must be less than or equal to 2000. #{a} was supplied"}   
+    end
+
+    def transform(%Drive{angle: a}) when
+      is_atom(a) and not (a in [:straight, :clockwise, :counter_clockwise]) do
+      {:error, 
+       "atom must be :straight, :clockwise, or :counter_clockwise. #{a} was supplied"}   
+    end
+
+    def transform(%Drive{speed: s, angle: a}) do
+      a = case a do
+        :straight -> 0x8000
+        :clockwise -> -1
+        :counter_clockwise -> 1
+        _ -> a
+      end
+    
+    
+      {:ok, << 137 :: size(1)-big-integer-unsigned-unit(8),
+               s :: size(2)-big-integer-signed-unit(8),
+               a :: size(2)-big-integer-signed-unit(8) >>}
+    end
+  end
+end
---
+++ test/command_test.exs
@@ -0,0 +1,106 @@
+defmodule CommandTest do
+  use ExUnit.Case
+  alias Roombex.Command
+  
+  test ":start translates to 128 single unsigned byte" do
+    expect = {:ok, << 128 :: size(1)-big-integer-unsigned-unit(8) >>}
+    result = Command.transform(:start)
+
+    assert result == expect
+  end
+
+  test ":control_mode transforms to 130 single unsigned byte" do
+    expect = {:ok, << 130 :: size(1)-big-integer-unsigned-unit(8) >>}
+    result = Command.transform(:control_mode)
+
+    assert result == expect
+  end
+
+  test ":safe_mode transforms to 131 single unsigned byte" do
+    expect = {:ok, << 131 :: size(1)-big-integer-unsigned-unit(8) >>}
+    result = Command.transform(:safe_mode)
+
+    assert result == expect
+  end
+
+  test ":full_mode transforms to 132 single unsigned byte" do
+    expect = {:ok, << 132 :: size(1)-big-integer-unsigned-unit(8) >>}
+    result = Command.transform(:full_mode)
+
+    assert result == expect
+  end
+  
+  test ":power transforms to 133 single unsigned byte"do
+    expect = {:ok, << 133 :: size(1)-big-integer-unsigned-unit(8) >>}
+    result = Command.transform(:power)
+
+    assert result == expect
+  end
+
+  test "anything else transforms to error"do
+    {result, _} = Command.transform(:foobar)
+
+    assert result == :error
+  end
+
+  test "Drive struct defaults produce resulting command with 0 speed and angle :straight" do
+    expect = {:ok, << 137 :: size(1)-big-integer-unsigned-unit(8),
+                      0 :: size(2)-big-integer-signed-unit(8),
+                      0x8000 :: size(2)-big-integer-signed-unit(8) >>}
+
+    result = Command.transform(%Drive{})
+    assert result == expect
+  end
+
+  test "drive with speed equal to max succeeds -- value used 500" do
+    expect = {:ok, << 137 :: size(1)-big-integer-unsigned-unit(8),
+              500 :: size(2)-big-integer-signed-unit(8),
+              0x8000 :: size(2)-big-integer-signed-unit(8) >>}
+
+    result  = Command.transform(%Drive{speed: 500})
+    assert result == expect
+  end 
+
+  test "drive with speed equal to max succeeds -- value used -500" do
+    expect = {:ok, << 137 :: size(1)-big-integer-unsigned-unit(8),
+              -500 :: size(2)-big-integer-signed-unit(8),
+              0x8000 :: size(2)-big-integer-signed-unit(8) >>}
+ 
+    result  = Command.transform(%Drive{speed: -500})
+    assert result == expect
+  end 
+
+  test "drive with angle equal to max succeeds -- value used -2000" do
+    expect = {:ok, << 137 :: size(1)-big-integer-unsigned-unit(8),
+              0 :: size(2)-big-integer-signed-unit(8),
+              -2000 :: size(2)-big-integer-signed-unit(8) >>}
+ 
+    result  = Command.transform(%Drive{angle: -2000})
+    assert result == expect
+  end 
+
+  test "Drive with speed greater than 500 fails -- value used 501" do
+    {result, _} = Command.transform(%Drive{speed: 501})
+    assert result == :error
+  end
+
+  test "Drive with speed less than -500 fails -- value used -501" do
+    {result, _} = Command.transform(%Drive{speed: -501})
+    assert result == :error
+  end
+
+  test "Drive with angle less than -2000 fails -- value used -2001" do
+    {result, _} = Command.transform(%Drive{angle: -2001})
+    assert result == :error
+  end
+
+  test "Drive with angle more than 2000 fails -- value used 2001" do
+    {result, _} = Command.transform(%Drive{angle: 2001})
+    assert result == :error
+  end
+
+  test "Drive with angle with atom not equal to :straight, :clockwise, :counter_clockwise fails -- value used :foobar" do
+    {result, _} = Command.transform(%Drive{angle: :foobar})
+    assert result == :error
+  end
+end
